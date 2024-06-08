3d7569f1d335369d0d10a7ac504e0c2a326732c6
Evgeny2295 <evgen22121995@mail.ru>
2024-06-08 21:13:18 +0700

    likes

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  data(){
+    return{
+      likes:0,
+      disLikes:0
+    }
+  },
+  methods:{
+    addLikes(){
+      this.likes +=1;
+    },
+    addDisLikes(){
+      this.disLikes +=1;
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <div>Количество лайков: {{likes}}</div>
+    <div>Количество дизлайков: {{disLikes}}</div>
+    <button v-on:click="addLikes">Нравится</button>
+    <button v-on:click="addDisLikes">не нравится</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
